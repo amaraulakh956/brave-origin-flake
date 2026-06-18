08c0ead2ba2366b9fb4658f2529ccd65aeb75582
Amar Aulakh <aulakh9523@gmail.com>
2026-06-18 14:39:27 -0400

    Add qt5 and qt6 dependencies to brave-origin-nightly

--- pkgs/brave-origin-nightly.nix
+++ pkgs/brave-origin-nightly.nix
@@ -2,7 +2,7 @@
 , gtk3, glib, nss, nspr, atk, cups, dbus, expat, libdrm
 , libX11, libXcomposite, libXdamage, libXext, libXfixes, libXrandr
 , mesa, libxkbcommon, pango, cairo, alsa-lib
-, at-spi2-atk, at-spi2-core }:
+, at-spi2-atk, at-spi2-core, qt5, qt6 }:
 
 stdenv.mkDerivation rec {
   pname = "brave-origin-nightly";
@@ -20,20 +20,22 @@ stdenv.mkDerivation rec {
     libX11 libXcomposite libXdamage libXext libXfixes libXrandr
     mesa libxkbcommon pango cairo alsa-lib
     at-spi2-atk at-spi2-core
+    qt5.qtbase qt5.qtgui qt5.qtwayland
+    qt6.qtbase qt6.qtgui qt6.qtwayland
   ];
 
   unpackPhase = ''
     dpkg-deb --fsys-tarfile $src | tar x --no-same-permissions --no-same-owner
   '';
 
-installPhase = ''
-  mkdir -p $out/bin
-  mkdir -p $out/opt/brave.com/brave-origin-nightly
-  cp -r opt/brave.com/brave-origin-nightly/* $out/opt/brave.com/brave-origin-nightly/
-  ln -s $out/opt/brave.com/brave-origin-nightly/brave-origin-nightly $out/bin/brave-origin-nightly
-  mkdir -p $out/share
-  cp -r usr/share/* $out/share/ 2>/dev/null || true
-'';
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/opt/brave.com/brave-origin-nightly
+    cp -r opt/brave.com/brave-origin-nightly/* $out/opt/brave.com/brave-origin-nightly/
+    ln -s $out/opt/brave.com/brave-origin-nightly/brave-origin-nightly $out/bin/brave-origin-nightly
+    mkdir -p $out/share
+    cp -r usr/share/* $out/share/ 2>/dev/null || true
+  '';
 
   meta = {
     description = "Brave Origin Nightly - minimalist privacy browser";
